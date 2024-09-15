31180179f1ce677cb6d1e672ea6eae82d8ea3082
zErichGut <121794449+zErichGut@users.noreply.github.com>
2024-09-15 16:47:29 -0600

    work on interactive mode

--- oalg-top/src/OAlg/Homology/IO/Interactive.hs
+++ oalg-top/src/OAlg/Homology/IO/Interactive.hs
@@ -29,16 +29,25 @@ import Control.Exception
 
 import System.IO
 
+import Data.Typeable
 import Data.List ((++),reverse,zip,repeat,dropWhile,span,words)
-import Data.Foldable (toList)
-import Data.Either
+import Data.Foldable (toList,foldl)
 import Data.Char (isSpace)
+import qualified Data.Map.Strict as M
 
 import OAlg.Prelude hiding (Result(..), It)
 
-import OAlg.Entity.Natural hiding ((++))
+import OAlg.Data.Canonical
+import OAlg.Data.Constructable
+import OAlg.Data.Either
+
+import OAlg.Entity.Natural hiding ((++),S)
 import OAlg.Entity.Sequence.Set
 import OAlg.Entity.Sum
+
+import OAlg.Structure.Fibred
+import OAlg.Structure.Additive
+import OAlg.Structure.Multiplicative
 
 import OAlg.AbelianGroup.Definition
 
@@ -46,7 +55,323 @@ import OAlg.Homology.Definition as H
 import OAlg.Homology.Complex
 import OAlg.Homology.ChainComplex
 import OAlg.Homology.Chain
+import OAlg.Homology.Simplex
 
+import OAlg.Data.Symbol (Symbol)
+
+--------------------------------------------------------------------------------
+-- InteractiveException -
+
+-- | arithmetic exceptions which are sub exceptions of 'SomeOAlgException'.
+data InteractiveException
+  = UndefinedVariable String
+  deriving (Eq,Show)
+
+instance Exception InteractiveException where
+  toException   = oalgExceptionToException
+  fromException = oalgExceptionFromException
+
+--------------------------------------------------------------------------------
+-- Term -
+
+infixl 0 :>>
+  
+data Term
+  = Free String -- ^ free variable.
+  | Let String Term Term
+  | PrimitiveTerm PrimitiveTerm
+  | (:>>) Term Term -- ^ application
+  | (:+>) Term Term
+  | (:!>) Term Term
+  deriving (Show, Eq,Ord)
+
+data PrimitiveTerm
+  = ZTerm Z
+  | Card  -- ^ cardinality of a set.
+  | HGroupTerm -- ^ homology group
+  | DTerm -- ^ boundary operator
+  | D'Term -- ^ \'inverse\' boundary operator
+  | GenTerm GenTerm
+  deriving (Show,Eq,Ord)
+
+data GenTerm
+  = STerm -- ^ chains
+  | CTerm -- ^ cycles
+  | TTerm -- ^ cycles, generating homology group
+  | HTerm -- ^ homology class
+  deriving (Show,Eq,Ord)
+
+zTerm :: Z -> Term
+zTerm = PrimitiveTerm . ZTerm
+
+type Failure = String
+
+type E = Either Failure
+
+
+instance Validable Term where
+  valid = error "nyi"
+
+instance Entity Term
+
+type EnvV = M.Map String Term
+
+
+subst :: EnvV -> Term -> Term
+subst vs t = case t of
+  Free v -> case M.lookup v vs of
+    Just t'  -> subst vs t'
+    Nothing ->  t
+
+  Let a s t -> subst vs' t where
+    vs' = M.insert a s vs
+  
+  PrimitiveTerm p -> PrimitiveTerm p
+
+  f :>> x -> subst vs f :>> subst vs x
+  a :+> b -> subst vs a :+> subst vs b
+  k :!> a -> subst vs k :!> subst vs a
+
+
+data SomeChain x where
+  SomeChain     :: Attestable l => Chain Z l x -> SomeChain x
+  SomeChainZero :: Z -> SomeChain x  -- ^ for negative length
+
+spxSomeChain :: (Entity x, Ord x, Attestable l) => Simplex l x -> SomeChain x
+spxSomeChain = SomeChain . ch
+
+deriving instance (Entity x, Ord x) => Show (SomeChain x)
+
+instance (Entity x, Ord x) => Eq (SomeChain x) where
+  (==) = error "nyi"
+
+instance (Entity x, Ord x) => Ord (SomeChain x) where
+  compare = error "nyi"
+
+instance (Entity x, Ord x) => Validable (SomeChain x) where
+  valid (SomeChain c)     = valid c
+  valid (SomeChainZero z) = valid z
+
+instance (Entity x, Ord x) => Entity (SomeChain x)
+
+instance (Entity x, Ord x) => Fibred (SomeChain x) where
+  type Root (SomeChain x) = Z
+  root (SomeChain c) = inj $ lengthN $ any c where
+    any :: Attestable l => Chain Z l x -> Any l
+    any _ = attest
+
+chZero :: (Entity x, Ord x, Attestable l) => Any l -> Chain Z l x
+chZero _ = zero ()
+
+instance (Entity x, Ord x) => Additive (SomeChain x) where
+  zero l | 0 <= l = case someNatural (prj l) of
+                      SomeNatural l' -> SomeChain $ chZero l'
+         | 0 > l  = SomeChainZero l
+
+instance Ord AbElement where
+  AbElement a `compare` AbElement b = a `compare` b
+  
+data Value x
+  = ZValue Z
+  | GenSetValue GenTerm
+  | ChainMapValue Z (M.Map Z (SomeChain x))
+  | ChainValue Z (SomeChain x)
+  | HomologyClassMapValue Z (M.Map Z AbElement)
+  | HomologyClassValue Z AbElement
+  | HomologyGroupValue Z AbGroup
+  deriving (Show,Eq,Ord)
+
+instance (Entity x, Ord x) => Validable (Value x) where
+  valid = error "nyi"
+
+instance (Entity x, Ord x) => Entity (Value x)
+
+infixr 0 :->
+  
+data ValueType
+  = ZType
+  | SomeChainType
+  | SomeHomologyClassType
+  | ChainType Z
+  | HomologyClassType Z
+  | HomologyGroupType Z
+  | (:->) ValueType ValueType
+  deriving (Show, Eq, Ord)
+
+
+instance Validable ValueType where
+  valid t = case t of
+    ZType       -> SValid
+    _           -> error "nyi"
+
+instance Entity ValueType
+
+instance (Entity x, Ord x) => Fibred (Value x) where
+  type Root (Value x) = ValueType
+  root v = case v of
+    ZValue _                  -> ZType
+    GenSetValue t             -> case t of
+      HTerm                   -> ZType :-> ZType :-> SomeHomologyClassType
+      _                       -> ZType :-> ZType :-> SomeChainType
+    ChainMapValue k _         -> ZType :-> ChainType k
+    ChainValue k _            -> ChainType k
+    HomologyClassMapValue k _ -> ZType :-> HomologyClassType k
+    HomologyClassValue k _    -> HomologyClassType k
+    HomologyGroupValue k _    -> HomologyGroupType k
+
+instance (Entity x, Ord x) => OrdRoot (Value x)
+
+newtype SumValue x = SumValue (Sum Z (Value x)) deriving (Show,Eq,Ord)
+
+instance (Entity x, Ord x) => Validable (SumValue x) where
+  valid (SumValue s) = Label "SumValue" :<=>: valid s
+
+instance (Entity x, Ord x) => Entity (SumValue x)
+
+instance (Entity x, Ord x) => Exposable (SumValue x) where
+  type Form (SumValue x) = SumForm Z (Value x)
+  form (SumValue s) = form s
+
+instance (Entity x, Ord x) => Fibred (SumValue x) where
+  type Root (SumValue x) = ValueType
+  root (SumValue s) = root s
+
+svReduce :: (Entity x, Ord x) => SumForm Z (Value x) -> SumForm Z (Value x)
+svReduce s = case root s of
+  ZType -> case evalZ s of
+    Right s' -> S (ZValue s')
+    Left _   -> throw $ ImplementationError "svReduce"
+  _     -> s
+
+
+instance (Entity x, Ord x) => Constructable (SumValue x) where
+  make = SumValue . make . svReduce
+
+type EnvH n x = M.Map N (SomeHomology n x)
+
+getHomology :: Attestable k => EnvH n x -> Any k -> Maybe (Homology n k x)
+getHomology hs k = do
+  sh <- lengthN k `M.lookup` hs
+  case sh of
+    SomeHomology h@(Homology _ _ _ _) -> case eq k h of
+      Just Refl -> Just h
+      Nothing   -> throw $ ImplementationError "getHomology: inconsitent environment"
+  where eq :: (Attestable k, Attestable k') => Any k -> Homology n k' x -> Maybe (k :~: k')
+        eq _ _ = eqT 
+
+data Env x where
+  Env :: EnvV -> EnvH n x -> Term -> Env x
+
+initEnv :: (Entity x, Ord x, Attestable n) => Regular -> Complex n x -> Env x
+initEnv r c = Env (M.empty) mhs (PrimitiveTerm (ZTerm 0)) where
+  ChainHomology hs = homology r c
+  mhs = M.fromAscList ([0..] `zip` (reverse $ toList hs))
+
+evalFormPrimitive :: EnvH n x -> PrimitiveTerm -> E (SumForm Z (Value x))
+evalFormPrimitive hs p = case p of
+  ZTerm z   -> return $ S $ ZValue z
+  GenTerm t -> return $ S $ GenSetValue t
+
+genSetEmpty :: GenTerm -> Z -> Value x
+genSetEmpty t k = case t of
+  TTerm -> HomologyClassMapValue k M.empty
+  _     -> ChainMapValue k M.empty
+
+genSetChain :: (Entity x, Ord x) => Homology n k x -> Z -> Value x
+genSetChain h@(Homology _ _ _ _) k
+  = ChainMapValue k $ M.fromAscList ([0..] `zip` (amap1 spxSomeChain $ setxs $ hmgChainSet' h))
+
+genSetCycle :: (Entity x, Ord x) => Homology n k x -> Z -> Value x
+genSetCycle h@(Homology _ _ _ _) k
+  = ChainMapValue k $ M.fromAscList ([0..] `zip` (amap1 SomeChain $ setxs $ hmgCycleGenSet h))
+
+genSetT :: (Entity x, Ord x) => Homology n k x -> Z -> Value x
+genSetT h@(Homology _ _ _ _) k
+  = ChainMapValue k $ M.fromAscList ([0..] `zip` (amap1 SomeChain $ setxs $ hmgGroupGenSet h))
+
+genSetMinusOne :: (Entity x, Ord x) => Homology n N0 x -> GenTerm -> Value x
+genSetMinusOne h t = ChainMapValue (-1) $ case t of
+  STerm -> genS
+  CTerm -> genS                         -- d (-1) is zero
+  TTerm | lengthN genS' == 0 -> genS    -- d 0 is zero
+        | otherwise          -> M.empty -- d 0 is surjective
+  
+  where genS  = M.fromAscList ([0..] `zip` (amap1 spxSomeChain $ setxs $ hmgChainSet h))
+        genS' = hmgChainSet' h
+  
+evalGenSet :: (Entity x, Ord x) => EnvH n x -> GenTerm -> Z -> Value x
+evalGenSet hs t k
+  | k == -1 = let Just h0 = getHomology hs W0 in genSetMinusOne h0 t  -- hs is never emtpy.
+  | -2 >  k = genSetEmpty t k
+  | k >=  0 = case (prj k) `M.lookup` hs of
+      Nothing               -> genSetEmpty t k
+      Just (SomeHomology h) -> case t of
+        STerm               -> genSetChain h k
+        CTerm               -> genSetCycle h k
+        TTerm               -> genSetT h k
+
+evalChain :: (Entity x, Ord x) => Z -> Maybe (SomeChain x) -> Value x
+evalChain k (Just c) = ChainValue k c
+evalChain k Nothing  = ChainValue k (zero (k+1)) 
+
+evalFormApplZ :: (Entity x, Ord x) => EnvH n x
+  -> SumForm Z (Value x) -> Z -> E (SumForm Z (Value x))
+evalFormApplZ hs s z = case s of
+  S (GenSetValue t)      -> return $ S $ evalGenSet hs t z
+  S (ChainMapValue k cs) -> return $ S $ evalChain k (z `M.lookup` cs)
+  _                 -> Left ("not applicable value: " ++ show s)
+
+evalFormAppl :: (Entity x, Ord x) => EnvH n x
+  -> SumForm Z (Value x) -> SumForm Z (Value x) -> E (SumForm Z (Value x))
+evalFormAppl hs f x = case (root f, root x) of
+  (ZType :-> _, ZType) -> evalZ x >>= evalFormApplZ hs f
+
+evalZ :: (Entity x, Ord x) => SumForm Z (Value x) -> E Z
+evalZ f = amap1 (foldl (+) 0) $ sequence $ amap1 (uncurry zMlt) $ lcs $ smflc f where
+  
+  zMlt :: (Entity x, Ord x) => Z -> Value x -> E Z
+  zMlt r v = case v of
+    ZValue x -> Right (r*x)
+    _        -> Left ("not a Z-value: " ++ show v)
+
+-- | pre: all free variables are substitiuted.
+--   post: valid SumForm
+evalForm :: (Entity x, Ord x) => EnvH n x -> Term -> E (SumForm Z (Value x))
+evalForm hs t = case t of
+  Free a -> Left ("unbound variable: " ++ a)
+
+  Let v _ _ -> Left ("unresolved \'let\' expresion: " ++ show v)
+
+  a :+> b -> do
+    a' <- evalForm hs a
+    b' <- evalForm hs b
+    case root a' == root b' of
+      True  -> return (a' :+ b')
+      False -> Left ("not addable: " ++ show (a,b))
+
+  z :!> a -> do
+    z' <- evalForm hs z >>= evalZ
+    a' <- evalForm hs a
+    return (z' :! a')
+
+
+  PrimitiveTerm p -> evalFormPrimitive hs p
+
+  f :>> x -> do
+    f' <- evalForm hs f
+    x' <- evalForm hs x
+    evalFormAppl hs f' x' 
+
+
+eval :: (Entity x, Ord x) => Env x -> Term -> E (SumValue x)
+eval (Env vs hs itTerm) t
+  = evalForm hs (subst vs (Let "it" itTerm t)) >>= return . make
+
+ee :: (Entity x, Ord x) => Env x -> Term -> E (SumForm Z (Value x))
+ee (Env vs hs itTerm) t
+  = evalForm hs (subst vs (Let "it" itTerm t))
+
+{-
 --------------------------------------------------------------------------------
 -- version -
 
@@ -418,3 +743,4 @@ iComplex' hIn hOut hErr r c = rep hks where
         hPutStrLn hOut "!!!unknown command"
         rep hks
 
+-}
