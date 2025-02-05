d1548d9b60a296ebaf9c79c2b682e58f9726a67e
zErichGut <121794449+zErichGut@users.noreply.github.com>
2025-02-05 02:57:02 -0600

    work on Homology Defintion

--- oalg-top/src/OAlg/Homology/Definition.hs
+++ oalg-top/src/OAlg/Homology/Definition.hs
@@ -48,6 +48,7 @@ import OAlg.Limes.Exact.ConsZero
 
 import OAlg.Hom.Definition
 import OAlg.Hom.Oriented
+import OAlg.Hom.Multiplicative
 import OAlg.Hom.Distributive
 
 import OAlg.AbelianGroup.Definition
@@ -60,71 +61,156 @@ import OAlg.Homology.ChainComplex
 import OAlg.Homology.SomeComplex.Definition
 
 --------------------------------------------------------------------------------
-
-
-
-
---------------------------------------------------------------------------------
 -- Homology -
 
 type Homology n = Deviation (n+1)
 
-{-
+--------------------------------------------------------------------------------
+-- HomologyOperatorAtom -
+
+data HomologyOperatorAtom n r s a b where
+  ChainComplex :: SimplexType s -> Regular -> Any n
+    -> HomologyOperatorAtom n r s SomeComplex (ChainComplex n (ChainOperator r s))
+  ScpxCards :: Any n ->  HomologyOperatorAtom n r s SomeComplex (Cards (n+3))
+  CcpRepMatrix
+    :: HomologyOperatorAtom n r s (ChainComplex n (ChainOperator r s)) (ChainComplex n (Matrix r))
+  CcpMap :: Hom Dst h => h (Matrix r) a
+    -> HomologyOperatorAtom n r s (ChainComplex n (Matrix r)) (ChainComplex n a)
+  Deviations :: Distributive a => Kernels N1 a -> Cokernels N1 a
+    -> HomologyOperatorAtom n r s (ChainComplex n a) (Homology n a)
+  CcpCards :: HomologyOperatorAtom n r s (ChainComplex n (ChainOperator r s)) (Cards (n+3))
+
+instance (AlgebraicRing r, Ord r, Typeable s, Typeable n)
+  => Morphism (HomologyOperatorAtom n r s) where
+  type ObjectClass (HomologyOperatorAtom n r s) = Ent
+  homomorphous (ChainComplex _ _ _) = Struct :>: Struct
+  homomorphous (ScpxCards _)        = Struct :>: Struct
+  homomorphous CcpRepMatrix         = Struct :>: Struct
+  homomorphous (CcpMap h)           = case  dstRng h of Struct -> Struct :>: Struct
+    where dstRng :: Hom Dst h => h a b -> Struct Dst b
+          dstRng = tau . range
+  homomorphous (Deviations _ _)     = Struct :>: Struct
+  homomorphous CcpCards             = Struct :>: Struct
+
+instance (AlgebraicRing r, Ord r, Typeable s)
+  => Applicative (HomologyOperatorAtom n r s) where
+  amap (ChainComplex s r n)     = \(SomeComplex c) -> chainComplex (structSmpl s c) r n c
+  amap (ScpxCards n)            = scpxCards n
+  amap CcpRepMatrix             = ccpRepMatrix
+  amap (CcpMap h)               = case range h of Struct -> cnzMap h
+  amap (Deviations kers cokers) = deviations kers cokers 
+  amap CcpCards                 = ccpCards
+
+--------------------------------------------------------------------------------
+-- HomologyOperator -
+
+type HomologyOperator n r s = Path (HomologyOperatorAtom n r s)
+
+--------------------------------------------------------------------------------
+-- chainComplexOpr -
+
+chainComplexOpr :: SimplexType s -> Regular -> Any n
+  -> HomologyOperator n r s SomeComplex (ChainComplex n (ChainOperator r s))
+chainComplexOpr s r n = ChainComplex s r n :. IdPath Struct
+
+--------------------------------------------------------------------------------
+-- scpxCardsOpr -
+
+scpxCardsOpr :: Any n -> HomologyOperator n r s SomeComplex (Cards (n+3))
+scpxCardsOpr n = ScpxCards n :. IdPath Struct
+
+scpxCardsOpr' :: Any n ->  HomologyOperator n Z Set SomeComplex (Cards (n+3))
+scpxCardsOpr' = scpxCardsOpr
+
+--------------------------------------------------------------------------------
+-- ccpRepMatrixOpr -
+
+ccpRepMatrixOpr :: (AlgebraicRing r, Ord r, Typeable s, Typeable n)
+  => HomologyOperator n r s (ChainComplex n (ChainOperator r s)) (ChainComplex n (Matrix r))
+ccpRepMatrixOpr = CcpRepMatrix :. IdPath Struct
+
+--------------------------------------------------------------------------------
+-- ccpMapOpr -
+
+ccpMapOpr :: (AlgebraicRing r, Typeable n, Hom Dst h)
+  => h (Matrix r) a
+  -> HomologyOperator n r s (ChainComplex n (Matrix r)) (ChainComplex n a)
+ccpMapOpr h = CcpMap h :. IdPath Struct
+
+--------------------------------------------------------------------------------
+-- deviationsOpr -
+
+deviationsOpr :: (Distributive a, Typeable n)
+  => Kernels N1 a -> Cokernels N1 a
+  -> HomologyOperator n r s (ChainComplex n a) (Homology n a)
+deviationsOpr kers cokers = Deviations kers cokers :. IdPath Struct
+
+--------------------------------------------------------------------------------
+-- ccpCardsOpr -
+
+ccpCardsOpr :: (AlgebraicRing r, Ord r, Typeable s, Typeable n)
+  => HomologyOperator n r s (ChainComplex n (ChainOperator r s)) (Cards (n+3))
+ccpCardsOpr = CcpCards :. IdPath Struct
+
+--------------------------------------------------------------------------------
+-- homologyOpr -
+
+homologyOpr :: ( AlgebraicRing r, Ord r, Typeable s, Typeable n
+               , Hom Dst h, Distributive a
+               )
+  => SimplexType s -> Regular -> Any n
+  -> h (Matrix r) a -> Kernels N1 a -> Cokernels N1 a
+  -> HomologyOperator n r s SomeComplex (Homology n a)
+homologyOpr s r n h kers cokers
+  = deviationsOpr kers cokers . ccpMapOpr h . ccpRepMatrixOpr . chainComplexOpr s r n
+
 --------------------------------------------------------------------------------
 -- homology -
 
-homology :: ( Hom Dst h
-            , AlgebraicSemiring r, Ring r, Ord r, Typeable s
-            , Distributive a
-            )
-  => h (Matrix r) a -> Kernels N1 a -> Cokernels N1 a
-  -> ChainComplex n (ChainOperator r s) -> Homology n a
-homology h kers cokers = deviations kers cokers . cnzMap h . ccpRepMatrix
+-- | the clasical homology 
+homology :: Typeable n => Any n -> SomeComplex -> Homology n AbHom
+homology n  = amap (homologyOpr SpxTypeSet Regular n h kers cokers) where
+  h      = FreeAbHom
+  kers   = abhKernels
+  cokers = abhCokernels
+
+--------------------------------------------------------------------------------
+
+s = complex [Set "ab", Set "bc",Set "ac"]
+t = cpxProductAsc s s
+
+--------------------------------------------------------------------------------
+-- ChainComplexTrafoOperatorAtom -
+
+data ChainComplexTrafoOperatorAtom n r s a b where
+  ChainComplexTrafo :: HomologyType s -> Regular -> Any n
+    -> ChainComplexTrafoOperatorAtom n r s (SomeComplexMap s) (ChainComplexTrafo n (ChainOperator r s))
+{-    
+  CcptCards :: ChainComplexTrafoOperatorAtom n r s
+                 (ChainComplexTrafo n (ChainOperator r s))
+                 (CardsTrafo (n+3))
 -}
 
---------------------------------------------------------------------------------
--- SomeComplexOperator -
+instance (AlgebraicRing r, Ord r, MultiplicativeComplexMap s, Typeable n)
+  => Morphism (ChainComplexTrafoOperatorAtom n r s) where
+  type ObjectClass (ChainComplexTrafoOperatorAtom n r s) = Mlt
+  homomorphous (ChainComplexTrafo _ _ _) = Struct :>: Struct
 
-data SomeComplexOperator n r s x y where
-  ChainComplex :: SimplexType s -> Regular -> Any n
-    -> SomeComplexOperator n r s SomeComplex (ChainComplex n (ChainOperator r s))
-  ScpxCards :: Any n ->  SomeComplexOperator n r s SomeComplex (Cards (n+3))
+instance (AlgebraicRing r, Ord r)
+  => Applicative (ChainComplexTrafoOperatorAtom n r s) where
+  amap (ChainComplexTrafo s r n) (SomeComplexMap f) = case entOrdComplexMap f of
+    (Struct,Struct) -> chainComplexTrafo (structHmlg s f) r n f
 
-instance (AlgebraicRing r, Ord r, Typeable s, Typeable n)
-  => Morphism (SomeComplexOperator n r s) where
-  type ObjectClass (SomeComplexOperator n r s) = Ent
-  homomorphous (ChainComplex _ _ _) = Struct :>: Struct
-  homomorphous (ScpxCards _)        = Struct :>: Struct
+cctoho :: ChainComplexTrafoOperatorAtom n r s a b -> HomologyOperatorAtom n r s (Point a) (Point b)
+cctoho (ChainComplexTrafo s r n) = ChainComplex (inj s) r n
 
---------------------------------------------------------------------------------
--- ChainComplexOperator -
+instance (AlgebraicRing r, Ord r, MultiplicativeComplexMap s, Typeable n)
+  => HomOriented (ChainComplexTrafoOperatorAtom n r s) where
+  pmap t = amap $ cctoho t
 
-data ChainComplexOperator n r s x y where
-  CcpRepMatrix
-    :: ChainComplexOperator n r s (ChainComplex n (ChainOperator r s)) (ChainComplex n (Matrix r))
-  CnzMap :: Hom Dst h => h (Matrix r) a
-    -> ChainComplexOperator n r s (ChainComplex n (Matrix r)) (ChainComplex n a)
-  Homology :: Distributive a => Kernels N1 a -> Cokernels N1 a
-    -> ChainComplexOperator n r s (ChainComplex n a) (Homology n a)
-  CppCards :: ChainComplexOperator n r s (ChainComplex n (ChainOperator r s)) (Cards (n+3))
+instance (AlgebraicRing r, Ord r, MultiplicativeComplexMap s, Typeable n)
+  => HomMultiplicative (ChainComplexTrafoOperatorAtom n r s)
 
-instance (AlgebraicRing r, Ord r, Typeable s, Typeable n)
-  => Morphism (ChainComplexOperator n r s) where
-  type ObjectClass (ChainComplexOperator n r s) = Ent
-  homomorphous CcpRepMatrix = Struct :>: Struct
-  homomorphous (CnzMap h)   = case  dstRng h of
-    Struct -> Struct :>: Struct
-    where dstRng :: Hom Dst h => h a b -> Struct Dst b
-          dstRng = tau . range
-  homomorphous (Homology _ _) = Struct :>: Struct
-  homomorphous CppCards = Struct :>: Struct
-
-instance (AlgebraicRing r, Ord r, Typeable s)
-  => Applicative (ChainComplexOperator n r s) where
-  amap CcpRepMatrix           = ccpRepMatrix
-  amap (CnzMap h)             = case range h of Struct -> cnzMap h
-  amap (Homology kers cokers) = deviations kers cokers 
-  amap CppCards               = ccpCards
 
 
 {-
