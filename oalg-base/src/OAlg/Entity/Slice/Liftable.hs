ecbb6231679871f989a7379fd483816936b90642
zErichGut <121794449+zErichGut@users.noreply.github.com>
2025-02-15 03:55:34 -0600

    work on Liftable

--- oalg-base/src/OAlg/Entity/Slice/Liftable.hs
+++ oalg-base/src/OAlg/Entity/Slice/Liftable.hs
@@ -11,7 +11,7 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DataKinds #-}
 
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, PolyKinds #-}
 
 
 -- |
@@ -24,12 +24,14 @@
 -- liftable slices.
 module OAlg.Entity.Slice.Liftable
   (
+{-    
     -- * Liftables
     Liftable(..), liftBase, lift
   , LiftableException(..)
 
     -- ** Duality
   , coLiftable
+-}
   ) where
 
 import Control.Monad
@@ -76,74 +78,94 @@ instance Exception LiftableException where
 
 -- | liftable slices.
 --
--- __Property__ Let @l@ be in @'Liftable' __p__ __i__ __c__@ for an @__i__@-sliced 'Oriented'
+-- __Property__ Let @l@ be in @'Liftable' __p__ __i__ __c__@ for an @__i__@-sliced 'Mulitiplicative'
 -- structure @__c__@, then holds:
 --
--- (1) If @l@ matches @'LiftableFrom' c lift@, then holds:
--- For all @f@ in @'Slice' 'From' __i__ __c__@ holds:
---
---     (1) If @'end' c '/=' 'end' ('slice' f)@ then the evaluation of @lift f@ ends up in a
---     'NotLiftable'-exception.
---
---     (2) If @'end' c '==' 'end' ('slice' f)@ then @lift f@ is 'valid' and
---     @'slice' f '==' c '*' 'slice' (lift f)@.
---
--- (2) If @l@ matches @'LiftableTo' c lift@, then holds:
+-- (1) If @l@ matches @'LiftableProjective' c lft@, then holds:
 -- For all @t@ in @'Slice' 'To' __i__ __c__@ holds:
 --
---     (1) If @'start' c '/=' 'start' ('slice' t)@ then the evaluation of @lift t@ ends up in a
+--     (1) If @'start' c '/=' 'start' ('slice' t)@ then the evaluation of @lft t@ ends up in a
 --     'NotLiftable'-exception.
 --
---     (2) If @'start' c '==' 'start' ('slice' t)@ then @lift t@ is 'valid' and
---     @'slice' t '==' 'slice' (lift l) '*' c@.
-data Liftable s i c where
-  LiftableFrom :: Sliced i c => c -> (Slice From i c -> Slice From i c) -> Liftable From i c
-  LiftableTo :: Sliced i c => c -> (Slice To i c -> Slice To i c) -> Liftable To i c
+--     (2) If @'start' c '==' 'start' ('slice' t)@ then holds: Let @t' = lft t@ in
+--
+--         (1) @t'@ is 'valid'.
+--
+--         (2) @'start' ('slice' t') '==' 'end' c@,
+--
+--         (3) @'slice' t '==' 'slice' t' '*' c@.
+--
+-- (2) If @l@ matches @'LiftableIjective' c lft@, then holds:
+-- For all @f@ in @'Slice' 'From' __i__ __c__@ holds:
+--
+--     (1) If @'end' c '/=' 'end' ('slice' f)@ then the evaluation of @lft f@ ends up in a
+--     'NotLiftable'-exception.
+--
+--     (2) If @'end' c '==' 'end' ('slice' f)@, then holds: Let @f' = lft f@ in
+--
+--         (1) @f'@ is 'valid'.
+--
+--         (2) @'end' ('slice' f') '==' 'start' c@.
+--
+--         (3) @'slice' f '==' c '*' 'slice' f'@.
+--
+data Liftable p i c where
+  LiftableProjective :: c -> (Slice To i c -> Slice To i c) -> Liftable Projective i c
+  LiftableInjective :: c -> (Slice From i c -> Slice From i c) -> Liftable Injective i c
+
 
 instance Show c => Show (Liftable s i c) where
-  show (LiftableFrom c _) = join ["LiftableFrom (",show c,") lift"]
-  show (LiftableTo c _)   = join ["LiftableTo (",show c,") lift"]
+  show (LiftableProjective c _) = join ["LiftableProjective (",show c,") lft"]
+  show (LiftableInjective c _)  = join ["LiftableInjective (",show c,") lft"]
 
 --------------------------------------------------------------------------------
 -- Liftable - Dual -
 
-type instance Dual (Liftable s i c) = Liftable (Dual s) i (Op c)
+type instance Dual (Liftable p i c) = Liftable (Dual p) i (Op c)
 
-coLiftable :: Dual (Dual s) :~: s -> Liftable s i c -> Dual (Liftable s i c)
-coLiftable r (LiftableFrom c lift) = LiftableTo (Op c) (coSlice . lift . coSliceInv r)
-coLiftable r (LiftableTo c lift) = LiftableFrom (Op c) (coSlice . lift . coSliceInv r)
+coLiftable :: Sliced i c => Liftable p i c -> Dual (Liftable p i c)
+coLiftable (LiftableProjective c lft) = LiftableInjective (Op c) (coSlice . lft . coSliceInv Refl)
+coLiftable (LiftableInjective c lft)  = LiftableProjective (Op c) (coSlice . lft . coSliceInv Refl)
 
 --------------------------------------------------------------------------------
 -- Liftable - Valid -
 
-relLiftableFrom :: Multiplicative c => i c -> XOrtOrientation c -> Liftable From i c -> Statement
-relLiftableFrom i xo (LiftableFrom c lift)
-  = And [ Label "c" :<=>: valid c
-        , Forall xf (\f
-            -> And [ Label "f" :<=>: valid f
-                   , let f' = lift f in case end c == end (slice f) of
-                       False -> (valid f' :=> throw implError)
+relLiftableProjective :: (Sliced i c, Multiplicative c)
+  => i c -> XOrtOrientation c -> Liftable Projective i c -> Statement
+relLiftableProjective i xo (LiftableProjective c lft)
+  = And [ valid c        
+        , Forall xt (\t
+            -> And [ valid t
+                   , let t' = lft t in case start c == start (slice t) of
+                       False -> Label "1.1" :<=>: (valid t' :=> throw implError)
                                   `Catch` (\e -> case e of NotLiftable -> SValid)
-                       True  -> (slice f == c * slice f')
-                                  :?> Params ["c":=show c,"f":=show f,"lift f":=show f']
+                                       
+                       True  -> And [ Label "1.2.1" :<=>: valid t'
+                                    , Label "1.2.2" :<=>: (start (slice t') == end c)
+                                        :?> Params ["c":=show c,"t":=show t,"t'":=show t']
+                                    , Label "1.2.3" :<=>: (slice t == slice t' * c)
+                                        :?> Params ["c":=show c,"t":=show t,"t'":=show t']
+                                    ]
+
                    ]
                     )
+
         ]
     
   where implError = ImplementationError "unliftable dos not throw a NotLiftable-exception"
         ip = slicePoint i
   
-        xf = amap1 (SliceFrom i)
-           $ xOneOfXW [ (9,xoArrow xo (ip :> end c))
-                      , (1,xoPoint xo >>= xoArrow xo . (ip:>))
+        xt = amap1 (SliceTo i)
+           $ xOneOfXW [ (9,xoArrow xo (start c :> ip))
+                      , (1,xoPoint xo >>= xoArrow xo . (:>ip))
                       ]
 
-relLiftable :: Multiplicative c => XOrtOrientation c -> Liftable s i c -> Statement
+relLiftable :: (Sliced i c, Multiplicative c) => XOrtOrientation c -> Liftable p i c -> Statement
 relLiftable xo l = case l of
-  LiftableFrom _ _ -> relLiftableFrom unit1 xo l
-  LiftableTo _ _   -> relLiftable (coXOrtOrientation xo) (coLiftable Refl l)
+  LiftableProjective _ _ -> relLiftableProjective unit1 xo l
+  LiftableInjective _ _  -> relLiftable (coXOrtOrientation xo) (coLiftable l)
   
-instance (Multiplicative c, XStandardOrtOrientation c)
+instance (Sliced i c, Multiplicative c, XStandardOrtOrientation c)
   => Validable (Liftable s i c) where
   valid l = Label "Liftable" :<=>: relLiftable xStandardOrtOrientation l
                                       
@@ -152,36 +174,93 @@ instance (Multiplicative c, XStandardOrtOrientation c)
 
 -- | the underlying factor.
 liftBase :: Liftable s i c -> c
-liftBase (LiftableFrom c _) = c
-liftBase (LiftableTo c _) = c
+liftBase (LiftableProjective c _) = c
+liftBase (LiftableInjective c _)  = c
 
 --------------------------------------------------------------------------------
 -- lift -
+type family ToSite (s :: k) :: Site
+
+type instance ToSite Projective = To
+type instance ToSite Injective = From
 
 -- | the lifting map.
-lift :: Liftable s i c -> Slice s i c -> Slice s i c
-lift (LiftableFrom _ l) = l
-lift (LiftableTo _ l) = l
+lift :: Liftable p i c -> Slice (ToSite p) i c -> Slice (ToSite p) i c
+lift (LiftableProjective _ l) = l
+lift (LiftableInjective _ l)  = l
 
 --------------------------------------------------------------------------------
 -- LiftableLimes -
 
 -- | liftable kernel respectively cokernel.
 --
--- __Properties__ Let @l@ be in @'LiftableLimes' __i__ __s__ __p__ __t__ __n__ __m__ __c__@,
--- then holds:
---
--- (1) If @l@ matches @'LiftableKernel' ker lft@, then holds:
--- @'kernelFactor' ('universalCone' ker) '==' 'liftBase' lft@.
---
--- (2) If @l@ matches @'LifteableCokernel' coker lft@, then holds:
--- @'cokernelFactor' ('universalCone' coker) '==' 'lftBase' lft@.
+-- __Property__ Let @l@ be in @'LiftableLimes' __i__ __s__ __p__ __t__ __n__ __m__ __c__@ for a
+-- 'Distributive' structure @__c__@, then holds: @'lmLiftable' l@ is 'valid'.
 data LiftableLimes i s p t n m c where
   LiftableKernel
-    :: Kernel N1 c -> Liftable To i c
+    :: Kernel N1 c -> (Slice To i c -> Slice To i c)
     -> LiftableLimes i Dst Projective (Parallel LeftToRight) N2 N1 c
   LiftableCokernel
-    :: Cokernel N1 c -> Liftable From i c
+    :: Cokernel N1 c -> (Slice From i c -> Slice From i c)
     -> LiftableLimes i Dst Injective (Parallel RightToLeft) N2 N1 c
-    
+
+--------------------------------------------------------------------------------
+-- LiftableKernel -
+
+-- | liftable kenrel according to a slice index @__i__@.
+type LiftableKernel i = LiftableLimes i Dst Projective (Parallel LeftToRight) N2 N1
+
+--------------------------------------------------------------------------------
+-- LiftableCokernel -
+
+-- | liftable cokernel according to a slice index @__i__@.
+type LiftableCokernel i = LiftableLimes i Dst Injective (Parallel RightToLeft) N2 N1
+
+--------------------------------------------------------------------------------
+-- lmLiftable -
+
+-- | the associated 'Liftable'.
+lmLiftable :: LiftableLimes i s p t n m c -> Liftable p i c
+lmLiftable (LiftableKernel k lft)   = LiftableProjective (kernelFactor $ universalCone k) lft
+lmLiftable (LiftableCokernel c lft) = LiftableInjective (cokernelFactor $ universalCone c) lft
+
+--------------------------------------------------------------------------------
+-- lftKernel -
+
+-- | the liftable property of 'LiftableKernel'.
+lftKernel :: LiftableKernel i c -> Slice To i c -> Slice To i c
+lftKernel l = lift (lmLiftable l)
+
+--------------------------------------------------------------------------------
+-- lftCokernel -
+
+lftCokernel :: LiftableCokernel i c -> Slice From i c -> Slice From i c
+lftCokernel l = lift (lmLiftable l)
+
+--------------------------------------------------------------------------------
+-- LiftableLimes - Predicate -
+
+instance Oriented c => Show (LiftableLimes i s p t n m c) where
+  show (LiftableKernel k _)   = join ["LiftableKernel (", show k, ") lft"]
+  show (LiftableCokernel c _) = join ["LiftableCokernel (", show c, ") lft"]
   
+
+instance ( Distributive c, Sliced i c
+         , XStandardOrtSiteTo c, XStandardOrtSiteFrom c
+         , XStandardOrtOrientation c
+         ) => Validable (LiftableLimes i s p t n m c) where
+  valid l@(LiftableKernel k _)   = Label "LiftableKernel" :<=>: valid k && valid (lmLiftable l)
+  valid l@(LiftableCokernel c _) = Label "LiftableCokernel" :<=>: valid c && valid (lmLiftable l)
+
+--------------------------------------------------------------------------------
+-- LiftableLimes - Universal -
+
+instance Universal (LiftableLimes i) where
+  universalType (LiftableKernel _ _)   = UniversalProjective
+  universalType (LiftableCokernel _ _) = UniversalInjective
+
+  universalCone (LiftableKernel k _)   = universalCone k
+  universalCone (LiftableCokernel c _) = universalCone c
+
+  universalFactor (LiftableKernel k _)   = universalFactor k
+  universalFactor (LiftableCokernel c _) = universalFactor c
