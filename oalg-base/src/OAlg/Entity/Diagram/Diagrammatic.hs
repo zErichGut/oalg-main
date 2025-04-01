eee3fd3321280bb88facc8a03b68307f6ca6200b
zErichGut <121794449+zErichGut@users.noreply.github.com>
2025-04-01 06:42:33 -0600

    work on duality for Diagrammatic

--- oalg-base/src/OAlg/Entity/Diagram/Diagrammatic.hs
+++ oalg-base/src/OAlg/Entity/Diagram/Diagrammatic.hs
@@ -80,6 +80,117 @@ dgmTypeRefl = dgTypeRefl . diagram
 --------------------------------------------------------------------------------
 -- ApplicativeDiagrammatic -
 
+class (Diagrammatic d, Applicative1 h (d t n m), HomOriented h) => ApplicativeDiagrammatic h d t n m
+
+instance HomOriented h => ApplicativeDiagrammatic h Diagram t n m
+
+--------------------------------------------------------------------------------
+-- prpApplicativeDiagrammatic -
+
+prpApplicativeDiagrammatic :: (ApplicativeDiagrammatic h d t n m, Show (d t n m a))
+  => h a b -> X (d t n m a) -> Statement
+prpApplicativeDiagrammatic h xd = Prp "ApplicativeDiagrammatic" :<=>:
+  case tauOrt (range h) of
+    Struct -> Forall xd
+      (\d  -> (amap1 h (diagram d) == diagram (amap1 h d))
+                :?> Params ["d":=show d]
+      )
+
+--------------------------------------------------------------------------------
+-- FunctorialDiagrammatic -
+
+type FunctorialDiagrammatic h d t n m
+  = (ApplicativeDiagrammatic h d t n m, Functorial1 h (d t n m), FunctorialHomOriented h)
+
+--------------------------------------------------------------------------------
+-- CoDiagrammatic -
+
+-- | dualisable diagrammatic objects.
+--
+-- __Property__ Let @'CoDiagrammatic' __d__@ then holds:
+-- For all @q@ in @__q__ __i__ __o__@, @__a__@ and @s@ in @'Struct' __s__ __a__@ with
+-- @'SDualityOrietned' __q__ __s__ __i__ __o__@ holds:
+--
+-- (1) @'diagram' '.' 'coDiagrammatic' q s = 'coDiagram' q s '.' 'diagram'@. 
+class (Diagrammatic d, SDualityOriented q s i o) => CoDiagrammatic q s i o d where
+  coDiagrammatic ::  q i o -> Struct s a -> d t n m a -> d (Dual t) n m (o a)
+
+
+instance SDualityOriented q s i o => CoDiagrammatic q s i o Diagram where coDiagrammatic = coDiagram
+
+--------------------------------------------------------------------------------
+-- prpCoDiagrammatic -
+
+prpCoDiagrammatic :: (CoDiagrammatic q s i o d, Show (d t n m a))
+  => q i o -> Struct s a -> X (d t n m a) -> Statement
+prpCoDiagrammatic q s xd = Prp "CoDiagrammatic" :<=>:
+  case tauOrt (sdlTau q s) of
+    Struct -> Forall xd
+      (\d -> (coDiagrammatic q s (diagram d) == diagram (coDiagrammatic q s d))
+               :?> Params ["d":=show d]
+      )
+
+  
+--------------------------------------------------------------------------------
+-- SDualityDiagrammatic -
+
+class ( CoDiagrammatic q s i o d, FunctorialDiagrammatic i d t n m
+      , FunctorialDiagrammatic i d (Dual t) n m
+      )
+  => SDualityDiagrammatic q s i o d t n m 
+
+instance (SDualityOriented q s i o, FunctorialHomOriented i)
+  => SDualityDiagrammatic q s i o Diagram t n m
+
+--------------------------------------------------------------------------------
+-- prpSDualityDiagrammatic -
+
+prpSDualityDiagrammatic :: SDualityDiagrammatic q s i o d t n m
+  => (Eq (d t n m (o (o a))), Eq (d (Dual t) n m (o (o (o a)))))
+  => Show (d t n m a)
+  => q i o -> Struct s a -> X (d t n m a) -> Statement
+prpSDualityDiagrammatic q s xd = Prp "SDualityDiagrammatic" :<=>:
+  And [ Forall xd
+          (\d    -> case dgmTypeRefl d of
+            Refl -> (coDiagrammatic q s' (coDiagrammatic q s d) == amap1 r d)
+                      :?> Params ["d":=show d]
+          )
+      , Forall xd
+          (\d -> (coDiagrammatic q s'' (amap1 r d) == amap1 r'' (coDiagrammatic q s d))
+                   :?> Params ["d":=show d]
+          )
+      ]
+  where s'         = sdlTau q s
+        s''        = sdlTau q s'
+        Inv2 r _   = sdlRefl q s
+        Inv2 r'' _ = sdlRefl q s'
+
+--------------------------------------------------------------------------------
+-- DiagrammaticDuality -
+
+data DiagrammaticDuality q s i o a b where
+  DiagrammaticDuality
+    :: SDualityDiagrammatic q s i o d t n m
+    => q i o
+    -> Dual (Dual t) :~: t
+    -> DiagrammaticDuality q s i o (d t n m) (d (Dual t) n m)
+    
+instance BiFunctorial1 i (DiagrammaticDuality q s i o) where
+  fnc1Fst (DiagrammaticDuality _ _) = Functor1
+  fnc1Snd (DiagrammaticDuality _ _) = Functor1
+
+instance SReflexive s i o => SDuality1 (DiagrammaticDuality q s) s i o where
+  sdlToDual1Fst (DiagrammaticDuality q _)    = coDiagrammatic q
+  sdlToDual1Snd (DiagrammaticDuality q Refl) = coDiagrammatic q 
+
+ff :: DiagramDuality q s i o (Diagram t n m) (Dual1 (Diagram t n m))
+  -> DiagrammaticDuality q s i o (Diagram t n m) (Diagram (Dual t) n m)
+ff (DiagramDuality q r) = DiagrammaticDuality q r
+
+{-
+--------------------------------------------------------------------------------
+-- ApplicativeDiagrammatic -
+
 -- | applications on 'Diagrammatic' objects.
 --
 -- __Property__ For all @h@ in @__h__ __a__ __b__@ with @'ApplicativeDiagrammatic' __h__ __d__@ holds:
@@ -104,60 +215,35 @@ prpApplicativeDiagrammatic h xd = Prp "ApplicativeDiagrammatic" :<=>:
       )
   
   
---------------------------------------------------------------------------------
--- CoDiagrammatic -
 
--- | dualisable diagrammatic objects.
---
--- __Property__ Let @'CoDiagrammatic' __d__@ then holds:
--- For all @q@ in @__q__ __i__ __o__@, @__a__@ and @s@ in @'Struct' __s__ __a__@ with
--- @'SDualityOrietned' __q__ __s__ __i__ __o__@ holds:
---
--- (1) @'diagram' '.' 'coDiagrammatic' q s = 'coDiagram' q s '.' 'diagram'@. 
-class Diagrammatic d => CoDiagrammatic d where
-  coDiagrammatic :: SDualityOriented q s i o => q i o -> Struct s a
-                 -> d t n m a -> d (Dual t) n m (o a)
-
-instance CoDiagrammatic Diagram where coDiagrammatic = coDiagram
 
 --------------------------------------------------------------------------------
 -- prpCoDiagrammatic -
 
 -- | validity according to 'CoDiagrammatic'.
-prpCoDiagrammatic :: (CoDiagrammatic d, SDualityOriented q s i o, Show (d t n m a))
+prpCoDiagrammatic :: (CoDiagrammatic q s i o d, Show (d t n m a), Eq (d t n m (o (o a))))
   => q i o -> Struct s a -> X (d t n m a) -> Statement
 prpCoDiagrammatic q s xd = Prp "CoDiagrammatic" :<=>:
-  case tauOrt (sdlTau q s) of
-    Struct -> Forall xd
-      (\d  -> (diagram (coDiagrammatic q s d) == coDiagram q s (diagram d))
-                :?> Params ["d":=show d]
-      )
-  
---------------------------------------------------------------------------------
--- DiagrammaticDuality -
+  And [ case tauOrt (sdlTau q s) of
+          Struct -> Forall xd
+            (\d  -> (diagram (coDiagrammatic q s d) == coDiagram q s (diagram d))
+                      :?> Params ["d":=show d]
+            )
+      , case tauOrt (sdlTau q $ sdlTau q s) of
+          Struct   -> Forall xd
+            (\d    -> case dgmTypeRefl d of
+              Refl -> (coDiagrammatic q (sdlTau q s) (coDiagrammatic q s d) == dmap r d)
+                        :?> Params ["d":=show d]    
+            )
+      ]
+  where Inv2 r _ = sdlRefl q s
+      
 
-data DiagrammaticDuality q s i o a b where
-  DiagrammaticDuality
-    :: ( SDualityOriented q s i o
-       , ApplicativeDiagrammatic i d
-       , Functorial1 i (d t n m), Functorial1 i (d (Dual t) n m)
-       , CoDiagrammatic d
-       )
-    => q i o
-    -> Dual (Dual t) :~: t
-    -> DiagrammaticDuality q s i o (d t n m) (d (Dual t) n m)
-    
-instance BiFunctorial1 i (DiagrammaticDuality q s i o) where
-  fnc1Fst (DiagrammaticDuality _ _) = Functor1
-  fnc1Snd (DiagrammaticDuality _ _) = Functor1
+-}
 
-instance SReflexive s i o => SDuality1 (DiagrammaticDuality q s) s i o where
-  sdlToDual1Fst (DiagrammaticDuality q _)    = coDiagrammatic q
-  sdlToDual1Snd (DiagrammaticDuality q Refl) = coDiagrammatic q 
 
-ff :: DiagramDuality q s i o (Diagram t n m) (Dual1 (Diagram t n m))
-  -> DiagrammaticDuality q s i o (Diagram t n m) (Diagram (Dual t) n m)
-ff (DiagramDuality q r) = DiagrammaticDuality q r
+
+
 {-
 --------------------------------------------------------------------------------
 -- DiagrammaticApplicative1 -
