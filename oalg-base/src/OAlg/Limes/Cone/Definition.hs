ec8d1c84bb056fbd123d8a0eea8341b48a6c9cfe
zErichGut <121794449+zErichGut@users.noreply.github.com>
2025-08-08 03:40:04 -0600

    finalizing Cone

--- oalg-base/src/OAlg/Limes/Cone/Definition.hs
+++ oalg-base/src/OAlg/Limes/Cone/Definition.hs
@@ -2,27 +2,23 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs, StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
+{-# LANGUAGE GADTs, StandaloneDeriving #-}
 {-# LANGUAGE DataKinds, ConstraintKinds #-}
-{-# LANGUAGE FlexibleContexts, RankNTypes #-}
 
--- {-# LANGUAGE UndecidableInstances #-}
 
 -- |
 -- Module      : OAlg.Limes.Cone.Definition
--- Description : definition of cones over diagrams
+-- Description : definition of cones over diagrammatic objects.
 -- Copyright   : (c) Erich Gut
 -- License     : BSD3
 -- Maintainer  : zerich.gut@gmail.com
 -- 
--- definition of 'Cone's over 'Diagram's.
+-- definition of 'Cone's over 'Diagrammatic' objects.
 module OAlg.Limes.Cone.Definition
-  (
-
-    -- * Cone
+  ( -- * Cone
     Cone(..), diagrammaticObject
   , Perspective(..), cnMltOrDst, coneStruct
   , cnDiagramTypeRefl
@@ -35,56 +31,29 @@ module OAlg.Limes.Cone.Definition
   , DualisableConeMlt, DualisableConeDst
   , NaturalDiagrammaticObjectClassBiDual1
 
-
-{-  
-    -- ** Distributive
-  , cnZeroHead
-  , cnKernel, cnCokernel
-  , cnDiffHead
-  , ConeZeroHead(..)
-  , coConeZeroHead, czFromOpOp, coConeZeroHeadInv
-  
-    -- ** Duality
-  , ConeDuality(..)
-  , coCone, coConeInv, cnFromOpOp
-
-    -- * Cone Struct
-  , ConeStruct(..), cnStructOp, cnStructMlt, cnStruct
- 
-    -- * Orientation
-  , cnPrjOrnt, cnInjOrnt
-
-    -- * Chain
-  , cnPrjChainTo, cnPrjChainToInv
-  , cnPrjChainFrom, cnPrjChainFromInv
-  , FactorChain(..)
--}
+    -- * X
+  , xCnPrjOrnt, xCnPrjDstOrnt
+  , xCnInjOrnt, xCnInjDstOrnt
   ) where
 
 import Control.Monad
 
-import Data.Kind
 import Data.Typeable
 import Data.Array hiding (range)
 
 import OAlg.Prelude
 
-import OAlg.Category.NaturalTransformable
 import OAlg.Category.Dualisable
 import OAlg.Category.SDuality
 
 import OAlg.Data.Either
-import OAlg.Data.Relation
 
-import OAlg.Entity.Natural hiding ((++))
+import OAlg.Entity.Natural
 import OAlg.Entity.FinList
 import OAlg.Entity.Diagram
-import OAlg.Entity.Diagram.Diagrammatic
 
 import OAlg.Structure.Oriented
-import OAlg.Structure.Fibred
-import OAlg.Structure.FibredOriented
-import OAlg.Structure.Multiplicative as Mlt
+import OAlg.Structure.Multiplicative
 import OAlg.Structure.Additive
 import OAlg.Structure.Distributive
 
@@ -783,129 +752,78 @@ cnInjDstOrnt :: (Diagrammatic d, Entity p, t ~ Parallel RightToLeft, n ~ N2)
   => p -> d t n m (Orientation p) -> Cone Dst Injective d t n m (Orientation p)
 cnInjDstOrnt t d = ConeCokernel d (q:>t) where DiagramParallelRL _ q _ = diagram d
 
+--------------------------------------------------------------------------------
+-- xCnPrjOrnt -
 
-instance ( Entity p
-         , Diagrammatic d
-         , XStandard p, XStandard (d t n m (Orientation p))
-         ) => XStandard (Cone Mlt Projective d t n m (Orientation p)) where
-  xStandard = do
-    d <- xStandard
-    p <- xStandard
-    return (cnPrjOrnt p d)
+-- | randorm variable for projecive multiplicative cones over 'Orientation'.
+xCnPrjOrnt :: (Diagrammatic d, Entity p)
+  => X p -> X (d t n m (Orientation p)) -> X (Cone Mlt Projective d t n m (Orientation p))
+xCnPrjOrnt xp xd = do
+  d <- xd
+  p <- xp
+  return (cnPrjOrnt p d)
+  
+instance
+  ( Entity p
+  , Diagrammatic d
+  , XStandard p, XStandard (d t n m (Orientation p))
+  )
+  => XStandard (Cone Mlt Projective d t n m (Orientation p)) where
+  xStandard = xCnPrjOrnt xStandard xStandard
+
+--------------------------------------------------------------------------------
+-- xCnPrjDstOrnt -
+
+-- | randorm variable for projecive distributive cones over 'Orientation',
+xCnPrjDstOrnt ::
+  ( Diagrammatic d
+  , Entity p
+  , t ~ Parallel LeftToRight, n ~ N2
+  )
+  => X p -> X (d t n m (Orientation p)) -> X (Cone Dst Projective d t n m (Orientation p))
+xCnPrjDstOrnt xp xd = do
+  d <- xd
+  t <- xp
+  return (cnPrjDstOrnt t d)
 
 instance ( Entity p, t ~ Parallel LeftToRight, n ~ N2
          , Diagrammatic d
          , XStandard p, XStandard (d t n m (Orientation p))
          ) => XStandard (Cone Dst Projective d t n m (Orientation p)) where
-  xStandard = do
-    d <- xStandard
-    t <- xStandard
-    return (cnPrjDstOrnt t d)
-                                 
-instance ( Entity p
-         , Diagrammatic d
-         , XStandard p, XStandard (d t n m (Orientation p))
-         ) => XStandard (Cone Mlt Injective d t n m (Orientation p)) where
-  xStandard = do
-    d <- xStandard
-    p <- xStandard
-    return (cnInjOrnt p d)
+  xStandard = xCnPrjDstOrnt xStandard xStandard
 
+--------------------------------------------------------------------------------
+-- xCnInjOrnt -
+
+-- | randorm variable for injective multiplicative cones over 'Orientation',
+xCnInjOrnt :: (Entity p, Diagrammatic d)
+  => X p -> X (d t n m (Orientation p)) -> X (Cone Mlt Injective d t n m (Orientation p))
+xCnInjOrnt xp xd = do
+  d <- xd
+  p <- xp
+  return (cnInjOrnt p d)
+
+instance
+  ( Entity p
+  , Diagrammatic d
+  , XStandard p, XStandard (d t n m (Orientation p))
+  )
+  => XStandard (Cone Mlt Injective d t n m (Orientation p)) where
+  xStandard = xCnInjOrnt xStandard xStandard
+
+--------------------------------------------------------------------------------
+-- xCnInjDstOrnt -
+
+-- | randorm variable for injective distributive cones over 'Orientation',
+xCnInjDstOrnt ::(Entity p, t ~ Parallel RightToLeft, n ~ N2, Diagrammatic d)
+  => X p -> X (d t n m (Orientation p)) -> X (Cone Dst Injective d t n m (Orientation p))
+xCnInjDstOrnt xp xd = do
+  d <- xd
+  t <- xp
+  return (cnInjDstOrnt t d)
+  
 instance ( Entity p, t ~ Parallel RightToLeft, n ~ N2
          , Diagrammatic d
          , XStandard p, XStandard (d t n m (Orientation p))
          ) => XStandard (Cone Dst Injective d t n m (Orientation p)) where
-  xStandard = do
-    d <- xStandard
-    t <- xStandard
-    return (cnInjDstOrnt t d)
-
---------------------------------------------------------------------------------
--- FactorChain -
-{-
--- | predicate for a factor with 'end' point at the starting point of the given chain.
---
--- __Property__
---
--- (1) Let @'FactorChain' f d@ be in @'FactorChain' 'To'  __n__ __a__@
--- for a 'Multiplicative' structure @__a__@ then holds:
--- @'end' f '==' 'chnToStart' d@.
---
--- (2) Let @'FactorChain' f d@ be in @'FactorChain' 'From'  __n__ __a__@
--- for a 'Multiplicative' structure @__a__@ then holds:
--- @'end' f '==' 'chnFromStart' d@.
-data FactorChain s n a = FactorChain a (Diagram (Chain s) (n+1) n a)
-  deriving (Show,Eq)
-
-instance Oriented a => Validable (FactorChain To n a) where
-  valid (FactorChain f d)
-    = And [ valid f
-          , valid d
-          , end f .==. chnToStart d
-          ]
-
-instance Oriented a => Validable (FactorChain From n a) where
-  valid (FactorChain f d)
-    = And [ valid f
-          , valid d
-          , end f .==. chnFromStart d
-          ]
-
--- instance (Multiplicative a, Typeable n) => Entity (FactorChain To n a)
--- instance (Multiplicative a, Typeable n) => Entity (FactorChain From n a)
-
---------------------------------------------------------------------------------
--- cnPrjChainTo
-
--- | the induced 'Projective' cone with ending factor given by the given 'FactorChain'.
---
--- __Property__ Let @h = 'FactorChain' f d@ be in
--- @'FactorChain' 'To' __n__ __a__@ for a 'Multiplicative' structure @__a__@ and
--- @'ConeProjective' d' _ (_':|'..':|'c':|''Nil') = 'cnPrjChainTo' h@ then holds:
--- @d' '==' d@ and @c '==' f@.
-cnPrjChainTo :: Multiplicative a
-  => FactorChain To n a -> Cone Mlt Projective Diagram (Chain To) (n+1) n a
-cnPrjChainTo (FactorChain f d@(DiagramChainTo _ as))
-  = ConeProjective d (start f) (cmp f as) where
-  cmp :: Multiplicative a => a -> FinList n a -> FinList (n+1) a
-  cmp f Nil     = f:|Nil
-  cmp f (a:|as) = (a*c):|cs where cs@(c:|_) = cmp f as
-
---------------------------------------------------------------------------------
--- cnPrjChainToInv -
-
--- | the underlying factor chain of a projective chain to cone, i.e the inverse of
--- 'cnPrjChainToInv'.
-cnPrjChainToInv :: Cone Mlt Projective Diagram (Chain To) (n+1) n a -> FactorChain To n a
-cnPrjChainToInv (ConeProjective d _ cs) = FactorChain (f cs) d where
-  f :: FinList (n+1) a -> a
-  f (c:|Nil)       = c
-  f (_:|cs@(_:|_)) = f cs
-
---------------------------------------------------------------------------------
--- chPrjChainFrom -
-
--- | the induced 'Projective' cone with starting factor given by the given 'FactorChain'.
---
--- __Property__ Let @h = 'FactorChain' f d@ be in
--- @'FactorChain' 'From' __n__ __a__@ for a 'Multiplicative' structure @__a__@ and
--- @'ConeProjective' d' _ (c':|'_) = 'cnPrjChainFrom' h@ then holds:
--- @d' '==' d@ and @c '==' f@.
-cnPrjChainFrom :: Multiplicative a
-  => FactorChain From n a -> Cone Mlt Projective Diagram (Chain From) (n+1) n a
-cnPrjChainFrom (FactorChain f d@(DiagramChainFrom _ as))
-  = ConeProjective d (start f) (cmp f as) where
-  cmp :: Multiplicative a => a -> FinList n a -> FinList (n+1) a
-  cmp f Nil     = f:|Nil
-  cmp f (a:|as) = f :| cmp f' as where f' = a*f
-
---------------------------------------------------------------------------------
--- cnPrjChainFromInv -
-
--- | the underlying factor chain of a projective chain from cone, i.e. the inverse of
--- 'cnPrjChainFrom'.
-cnPrjChainFromInv :: Cone Mlt Projective Diagram (Chain From) (n+1) n a -> FactorChain From n a
-cnPrjChainFromInv (ConeProjective d _ (c:|_)) = FactorChain c d
--}
-
-
+  xStandard = xCnInjDstOrnt xStandard xStandard
--- oalg-base/src/OAlg/Limes/Cone/FactorChain.hs
+++ oalg-base/src/OAlg/Limes/Cone/FactorChain.hs
@@ -2,83 +2,37 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs, StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE DataKinds, ConstraintKinds #-}
-{-# LANGUAGE FlexibleContexts, RankNTypes #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DataKinds #-}
 
 -- |
 -- Module      : OAlg.Limes.Cone.FactorChain
--- Description : predicat for chains
+-- Description : consturction of a cones for chains.
 -- Copyright   : (c) Erich Gut
 -- License     : BSD3
 -- Maintainer  : zerich.gut@gmail.com
 -- 
--- predicate for a factor with 'end' point at the starting point of the given chain.
+-- predicate for a factor with 'end' point at the starting point of a given chain.
 module OAlg.Limes.Cone.FactorChain
-  (
-{-  
-    -- ** Distributive
-  , cnZeroHead
-  , cnKernel, cnCokernel
-  , cnDiffHead
-  , ConeZeroHead(..)
-  , coConeZeroHead, czFromOpOp, coConeZeroHeadInv
-  
-    -- ** Duality
-  , ConeDuality(..)
-  , coCone, coConeInv, cnFromOpOp
-
-    -- * Cone Struct
-  , ConeStruct(..), cnStructOp, cnStructMlt, cnStruct
- 
-    -- * Orientation
-  , cnPrjOrnt, cnInjOrnt
-
-    -- * Chain
+  ( -- * Chain
+    FactorChain(..)
   , cnPrjChainTo, cnPrjChainToInv
   , cnPrjChainFrom, cnPrjChainFromInv
-  , FactorChain(..)
--}
   ) where
-
-import Control.Monad
-
-import Data.Kind
-import Data.Typeable
-import Data.Array hiding (range)
 
 import OAlg.Prelude
 
-import OAlg.Category.NaturalTransformable
-import OAlg.Category.Dualisable
-import OAlg.Category.SDuality
-
-import OAlg.Data.Either
-import OAlg.Data.Relation
-
-import OAlg.Entity.Natural hiding ((++))
+import OAlg.Entity.Natural
 import OAlg.Entity.FinList
 import OAlg.Entity.Diagram
-import OAlg.Entity.Diagram.Diagrammatic
 
 import OAlg.Structure.Oriented
-import OAlg.Structure.Fibred
-import OAlg.Structure.FibredOriented
-import OAlg.Structure.Multiplicative as Mlt
-import OAlg.Structure.Additive
-import OAlg.Structure.Distributive
-
-import OAlg.Hom.Oriented
-import OAlg.Hom.Multiplicative
-import OAlg.Hom.Distributive
-import OAlg.Hom.Definition
+import OAlg.Structure.Multiplicative
 
 import OAlg.Limes.Perspective
 
-import OAlg.Limes.Cone.Structure
 import OAlg.Limes.Cone.Definition
 
 --------------------------------------------------------------------------------
@@ -97,11 +51,8 @@ import OAlg.Limes.Cone.Definition
 -- @'end' f '==' 'chnFromStart' ('diagram' d)@.
 data FactorChain d s n x = FactorChain x (d (Chain s) (n+1) n x)
 
-
 deriving instance (Show x, ShowPoint x) => Show (FactorChain Diagram s n x)
 deriving instance (Eq x, EqPoint x) => Eq (FactorChain Diagram s n x)
-
-
 
 instance (Diagrammatic d, Oriented x) => Validable (FactorChain d To n x) where
   valid (FactorChain f d)
@@ -121,7 +72,7 @@ instance (Diagrammatic d, Oriented x) => Validable (FactorChain d From n x) wher
 
 -- instance (Multiplicative a, Typeable n) => Entity (FactorChain To n a)
 -- instance (Multiplicative a, Typeable n) => Entity (FactorChain From n a)
-{-
+
 --------------------------------------------------------------------------------
 -- cnPrjChainTo
 
@@ -131,10 +82,11 @@ instance (Diagrammatic d, Oriented x) => Validable (FactorChain d From n x) wher
 -- @'FactorChain' 'To' __n__ __a__@ for a 'Multiplicative' structure @__a__@ and
 -- @'ConeProjective' d' _ (_':|'..':|'c':|''Nil') = 'cnPrjChainTo' h@ then holds:
 -- @d' '==' d@ and @c '==' f@.
-cnPrjChainTo :: Multiplicative a
-  => FactorChain To n a -> Cone Mlt Projective Diagram (Chain To) (n+1) n a
-cnPrjChainTo (FactorChain f d@(DiagramChainTo _ as))
-  = ConeProjective d (start f) (cmp f as) where
+cnPrjChainTo :: (Diagrammatic d, Multiplicative x)
+  => FactorChain d To n x -> Cone Mlt Projective d (Chain To) (n+1) n x
+cnPrjChainTo (FactorChain f d) = ConeProjective d (start f) (cmp f as) where
+  as = dgArrows $ diagram d
+
   cmp :: Multiplicative a => a -> FinList n a -> FinList (n+1) a
   cmp f Nil     = f:|Nil
   cmp f (a:|as) = (a*c):|cs where cs@(c:|_) = cmp f as
@@ -144,9 +96,9 @@ cnPrjChainTo (FactorChain f d@(DiagramChainTo _ as))
 
 -- | the underlying factor chain of a projective chain to cone, i.e the inverse of
 -- 'cnPrjChainToInv'.
-cnPrjChainToInv :: Cone Mlt Projective Diagram (Chain To) (n+1) n a -> FactorChain To n a
+cnPrjChainToInv :: Cone Mlt Projective d (Chain To) (n+1) n x -> FactorChain d To n x
 cnPrjChainToInv (ConeProjective d _ cs) = FactorChain (f cs) d where
-  f :: FinList (n+1) a -> a
+  f :: FinList (n+1) x -> x
   f (c:|Nil)       = c
   f (_:|cs@(_:|_)) = f cs
 
@@ -159,11 +111,12 @@ cnPrjChainToInv (ConeProjective d _ cs) = FactorChain (f cs) d where
 -- @'FactorChain' 'From' __n__ __a__@ for a 'Multiplicative' structure @__a__@ and
 -- @'ConeProjective' d' _ (c':|'_) = 'cnPrjChainFrom' h@ then holds:
 -- @d' '==' d@ and @c '==' f@.
-cnPrjChainFrom :: Multiplicative a
-  => FactorChain From n a -> Cone Mlt Projective Diagram (Chain From) (n+1) n a
-cnPrjChainFrom (FactorChain f d@(DiagramChainFrom _ as))
-  = ConeProjective d (start f) (cmp f as) where
-  cmp :: Multiplicative a => a -> FinList n a -> FinList (n+1) a
+cnPrjChainFrom :: (Diagrammatic d, Multiplicative x)
+  => FactorChain d From n x -> Cone Mlt Projective d (Chain From) (n+1) n x
+cnPrjChainFrom (FactorChain f d) = ConeProjective d (start f) (cmp f as) where
+  as = dgArrows $ diagram d
+  
+  cmp :: Multiplicative x => x -> FinList n x -> FinList (n+1) x
   cmp f Nil     = f:|Nil
   cmp f (a:|as) = f :| cmp f' as where f' = a*f
 
@@ -172,8 +125,8 @@ cnPrjChainFrom (FactorChain f d@(DiagramChainFrom _ as))
 
 -- | the underlying factor chain of a projective chain from cone, i.e. the inverse of
 -- 'cnPrjChainFrom'.
-cnPrjChainFromInv :: Cone Mlt Projective Diagram (Chain From) (n+1) n a -> FactorChain From n a
+cnPrjChainFromInv :: Cone Mlt Projective d (Chain From) (n+1) n x -> FactorChain d From n x
 cnPrjChainFromInv (ConeProjective d _ (c:|_)) = FactorChain c d
--}
+
 
 
