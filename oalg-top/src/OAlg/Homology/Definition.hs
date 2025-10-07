d65983f18e54c29ef6c3da57d3aad4cd91058842
zErichGut <121794449+zErichGut@users.noreply.github.com>
2025-10-07 14:42:12 -0600

    work on homology

--- oalg-top/src/OAlg/Homology/Definition.hs
+++ oalg-top/src/OAlg/Homology/Definition.hs
@@ -39,21 +39,26 @@ import OAlg.Data.Either
 import OAlg.Structure.Additive
 import OAlg.Structure.Multiplicative
 import OAlg.Structure.Exponential
+import OAlg.Structure.Operational
 
 import OAlg.Entity.Diagram hiding (Chain)
 import OAlg.Entity.Natural
 import OAlg.Entity.FinList
+import OAlg.Entity.Slice.Definition
 import OAlg.Entity.Slice.Free
 import OAlg.Entity.Sequence.Set
+import OAlg.Entity.Matrix.Vector
 
 import OAlg.Hom.Distributive
 
 import OAlg.AbelianGroup.Definition
 import OAlg.AbelianGroup.KernelsAndCokernels
 
+import OAlg.Limes.Definition
 import OAlg.Limes.Exact.ConsecutiveZero
 import OAlg.Limes.Exact.Deviation
 import OAlg.Limes.Exact.Free
+import OAlg.Limes.KernelsAndCokernels
 
 import OAlg.Homology.Simplical
 import OAlg.Homology.Complex
@@ -107,8 +112,9 @@ chainComplexFree' _ = chainComplexFree
 --------------------------------------------------------------------------------
 -- Homology -
 
+-- | homology for the simplex type @__s__@ over vertices of type @__x__@.
 data Homology s x where
-  Homology :: (Typeable x, Typeable s, Attestable n)
+  Homology :: (Simplical s x, Attestable n)
     => N -- actual dimension
     -> ChainComplex n (ChainOperator Z s)
     -> VarianceFreeLiftable To n AbHom
@@ -117,32 +123,49 @@ data Homology s x where
 --------------------------------------------------------------------------------
 -- homology -
 
+-- | the induced homology of a complex.
 homology :: Simplical s x => Regular -> N -> Complex x -> Homology s x
 homology r dMax c = case someNatural dMax of
   SomeNatural dMax' -> Homology 0 cos vfs where
     ChainComplexFree cos cf = chainComplexFree r dMax' c
     vfs = varianceFreeTo abhKernelsSomeFreeFreeTip abhCokernelsLiftableSomeFree cf
 
+-- | the induced homology of a complex according to the proxy type.
 homology' :: Simplical s x => q s -> Regular -> N -> Complex x -> Homology s x
 homology' _ = homology
 
 --------------------------------------------------------------------------------
+-- hmgCards -
+
+-- | the cardinalities of the simplex sets.
+hmgCards :: Homology s x -> [N]
+hmgCards (Homology _ cos _) = case cos of
+  ConsecutiveZero d -> toList $ amap1 lengthN $ dgPoints d
+
+--------------------------------------------------------------------------------
+-- attest2 -
+
+attest2 :: Attestable n => q n x -> Any n
+attest2 _ = attest
+
+--------------------------------------------------------------------------------
 -- hmgRange -
 
+-- | the range of the homology.
 hmgRange :: Homology s x -> (N,N)
-hmgRange (Homology i cos _) = (i,i + lengthN (d cos)) where
-  d :: Attestable n => q n x -> Any n
-  d _ = attest
+hmgRange (Homology i cos _) = (i,i + lengthN (attest2 cos))
 
 --------------------------------------------------------------------------------
 -- hmgIndex -
 
+-- | the actual index.
 hmgIndex :: Homology s x -> N
 hmgIndex = fst . hmgRange
 
 --------------------------------------------------------------------------------
 -- hmgGroups -
 
+-- | the homology groups.
 hmgGroups :: Homology s x -> [AbGroup]
 hmgGroups (Homology _ _ vfs) = toList $ dgPoints $ deviationsTo vfs
 
@@ -156,8 +179,10 @@ hmgGroup (Homology _ _ vfs) = head $ dgPoints $ deviationsTo vfs
 --------------------------------------------------------------------------------
 -- EvalFailure -
 
+-- | evaluation failures.
 data EvalFailure
   = NoSuccesor
+  | IndexOutOfRange N
   | EvalFailure String
   deriving (Show)
 
@@ -167,23 +192,34 @@ failure = Left
 --------------------------------------------------------------------------------
 -- Eval -
 
+-- | evaluation monad for homology.
 type Eval x = Either EvalFailure x
 
 --------------------------------------------------------------------------------
 -- hmgSucc -
 
+-- | increases the actual index by one.
 hmgSucc :: Homology s x -> Eval (Homology s x)
-hmgSucc (Homology i cos vfs) = case d cos of
+hmgSucc (Homology i cos vfs) = case attest2 cos of
   W0   -> failure NoSuccesor
   SW n -> case ats n of Ats -> return $ Homology (succ i) (cnzTail cos) (vrcTail vfs) 
-  where
-    d :: Attestable n => q n x -> Any n
-    d _ = attest
   
+--------------------------------------------------------------------------------
+-- hmgAt -
 
+-- | sets the index to the given one. 
+hmgAt :: N -> Homology s x -> Eval (Homology s x)
+hmgAt i hg | l <= i && i <= h = at l i hg
+           | otherwise        = failure $ IndexOutOfRange i
+  where (l,h) = hmgRange hg
+        at l i h | l == i    = return h
+                 | otherwise = hmgSucc h >>= at (succ l) i
+                  
 --------------------------------------------------------------------------------
 -- hmgSimplices -
 
+-- | the set of simplices at the actual index, which form a base for the free abelian group of chains
+-- of dimension given by the actual index.
 hmgSimplices :: Homology s x -> Eval (Set (s x))
 hmgSimplices h@(Homology _ cos _) = case cos of
   ConsecutiveZero cs         -> case cs of
@@ -195,11 +231,52 @@ hmgSimplices h@(Homology _ cos _) = case cos of
     eqType :: (Typeable x, Typeable y) => q x -> Set (s y) -> Maybe (x :~: y)
     eqType _ _ = eqT
 
+-----------------------------------------------------------------------------------------
+-- Chain -
+
+-- | chain of the given simplex type over the given vertex type.
+type Chain s x = ChainG Z s x
+
+--------------------------------------------------------------------------------
+-- abges -
+
+-- | list of the canonical generators
+abges :: AbGroup -> [AbElement]
+abges g = [abge g (pred i) | i <- [1..lengthN g]] 
+
+abeFreeFrom :: AbElement -> Slice From (Free N1) AbHom
+abeFreeFrom (AbElement f) = f
+
+--------------------------------------------------------------------------------
+-- hmgToChain -
+
+-- | the chain according to the actual simplex set and the given vector.
+--
+-- __Note__ The indices of the given vector which succed the cardinality of the given set,
+-- will be troped.
+hmgToChain :: Homology s x -> Vector Z -> Eval (Chain s x)
+hmgToChain h@(Homology _ _ _) v = do
+  ssx <- hmgSimplices h
+  return (cfsssy ssx v)
+
+--------------------------------------------------------------------------------
+-- hmg -
+
+-- | a base for the cycles at the actual index.
+hmgCycles :: Homology s x -> Eval [Chain s x]
+hmgCycles h@(Homology _ _ vfs)  = case vfs of
+  VarianceG aCos ((aKer,_):|_) -> case universalCone aKer of
+    ConicFreeTip _ aCn         -> sequence
+                                $ amap1 (hmgToChain h . abhvecFree1 . (k*>). abeFreeFrom)
+                                $ abges
+                                $ start k
+      where k = kernelFactor aCn
+
 
 --------------------------------------------------------------------------------
 
-c   = complex [Set [0..2]] :: Complex N
-h   = homology' (Proxy :: Proxy Set) Regular 5 c
+c   = complex [Set [0..4]] :: Complex N
+h   = homology' (Proxy :: Proxy Asc) Regular 5 c
 
 
 
@@ -219,11 +296,6 @@ hmgTail (Homology cos vf) = Homology (cnzTail cos) (vrcTail vf)
 
 hmgGroup :: Attestable n => Homology s n x -> AbGroup
 hmgGroup = head . dgPoints . hmgGroups
-
------------------------------------------------------------------------------------------
--- Chain -
-
-type Chain x = ChainG Z Set x
 
 --------------------------------------------------------------------------------
 -- hmgChains -
