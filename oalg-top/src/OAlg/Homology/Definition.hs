860cb5a7f09b189412755f5d8ebaa0b9b707e06b
zErichGut <121794449+zErichGut@users.noreply.github.com>
2025-10-07 11:13:26 -0600

    work on homology

--- oalg-top/src/OAlg/Homology/Definition.hs
+++ oalg-top/src/OAlg/Homology/Definition.hs
@@ -18,17 +18,25 @@
 -- homology.
 module OAlg.Homology.Definition
   (
+{-    
     -- * Homology
     Homology()
     
     -- * Chain Complex Free
   , ChainComplexFree()
+-}
   ) where
 
+import Control.Monad
+
 import Data.Typeable
+import Data.Foldable (toList)
 
 import OAlg.Prelude
 
+import OAlg.Data.Either
+
+import OAlg.Structure.Additive
 import OAlg.Structure.Multiplicative
 import OAlg.Structure.Exponential
 
@@ -99,19 +107,107 @@ chainComplexFree' _ = chainComplexFree
 --------------------------------------------------------------------------------
 -- Homology -
 
-data Homology s n x where
-  Homology :: Typeable x
-    => ChainComplex n (ChainOperator Z s)
+data Homology s x where
+  Homology :: (Typeable x, Typeable s, Attestable n)
+    => N -- actual dimension
+    -> ChainComplex n (ChainOperator Z s)
     -> VarianceFreeLiftable To n AbHom
-    -> Homology s n x
+    -> Homology s x
 
 --------------------------------------------------------------------------------
 -- homology -
 
-homology :: ChainComplexFree s n x -> Homology s n x
-homology (ChainComplexFree cos cf)
-  = Homology cos (varianceFreeTo abhKernelsSomeFreeFreeTip abhCokernelsLiftableSomeFree cf)
+homology :: Simplical s x => Regular -> N -> Complex x -> Homology s x
+homology r dMax c = case someNatural dMax of
+  SomeNatural dMax' -> Homology 0 cos vfs where
+    ChainComplexFree cos cf = chainComplexFree r dMax' c
+    vfs = varianceFreeTo abhKernelsSomeFreeFreeTip abhCokernelsLiftableSomeFree cf
 
+homology' :: Simplical s x => q s -> Regular -> N -> Complex x -> Homology s x
+homology' _ = homology
+
+--------------------------------------------------------------------------------
+-- hmgRange -
+
+hmgRange :: Homology s x -> (N,N)
+hmgRange (Homology i cos _) = (i,i + lengthN (d cos)) where
+  d :: Attestable n => q n x -> Any n
+  d _ = attest
+
+--------------------------------------------------------------------------------
+-- hmgIndex -
+
+hmgIndex :: Homology s x -> N
+hmgIndex = fst . hmgRange
+
+--------------------------------------------------------------------------------
+-- hmgGroups -
+
+hmgGroups :: Homology s x -> [AbGroup]
+hmgGroups (Homology _ _ vfs) = toList $ dgPoints $ deviationsTo vfs
+
+--------------------------------------------------------------------------------
+-- hmgGroup -
+
+-- | the homology group at the aktual index.
+hmgGroup :: Homology s x -> AbGroup
+hmgGroup (Homology _ _ vfs) = head $ dgPoints $ deviationsTo vfs
+
+--------------------------------------------------------------------------------
+-- EvalFailure -
+
+data EvalFailure
+  = NoSuccesor
+  | EvalFailure String
+  deriving (Show)
+
+failure :: EvalFailure -> Eval x
+failure = Left
+
+--------------------------------------------------------------------------------
+-- Eval -
+
+type Eval x = Either EvalFailure x
+
+--------------------------------------------------------------------------------
+-- hmgSucc -
+
+hmgSucc :: Homology s x -> Eval (Homology s x)
+hmgSucc (Homology i cos vfs) = case d cos of
+  W0   -> failure NoSuccesor
+  SW n -> case ats n of Ats -> return $ Homology (succ i) (cnzTail cos) (vrcTail vfs) 
+  where
+    d :: Attestable n => q n x -> Any n
+    d _ = attest
+  
+
+--------------------------------------------------------------------------------
+-- hmgSimplices -
+
+hmgSimplices :: Homology s x -> Eval (Set (s x))
+hmgSimplices h@(Homology _ cos _) = case cos of
+  ConsecutiveZero cs         -> case cs of
+    DiagramChainTo _ (d:|_)  -> case start d of
+      SimplexSet ssx         -> case eqType h ssx of
+        Just Refl            -> return ssx
+        Nothing              -> failure $ EvalFailure "implementation error at: hmgSimplices"
+  where
+    eqType :: (Typeable x, Typeable y) => q x -> Set (s y) -> Maybe (x :~: y)
+    eqType _ _ = eqT
+
+
+--------------------------------------------------------------------------------
+
+c   = complex [Set [0..2]] :: Complex N
+h   = homology' (Proxy :: Proxy Set) Regular 5 c
+
+
+
+{-
+(ChainComplexFree cos cf)
+  = Homology cos (varianceFreeTo abhKernelsSomeFreeFreeTip abhCokernelsLiftableSomeFree cf)
+-}
+{-
 --------------------------------------------------------------------------------
 -- hmgTail -
 
@@ -119,30 +215,10 @@ hmgTail :: Typeable s => Homology s (n+1) x -> Homology s n x
 hmgTail (Homology cos vf) = Homology (cnzTail cos) (vrcTail vf)
 
 --------------------------------------------------------------------------------
--- hmgGroups -
-
-hmgGroups :: Attestable n => Homology s n x -> Deviation (n+1) AbHom
-hmgGroups (Homology _ vfs) = deviationsTo vfs
-
---------------------------------------------------------------------------------
 -- hmgGroup -
 
 hmgGroup :: Attestable n => Homology s n x -> AbGroup
 hmgGroup = head . dgPoints . hmgGroups
-
---------------------------------------------------------------------------------
--- hmgSimplices -
-
-hmgSimplices :: Typeable s => Homology s n x -> Set (s x)
-hmgSimplices h@(Homology cos _) = case cos of
-  ConsecutiveZero cs         -> case cs of
-    DiagramChainTo _ (d:|_)  -> case start d of
-      SimplexSet ssx         -> case eqType h ssx of
-        Just Refl            -> ssx
-        Nothing              -> throw $ ImplementationError "hmgSimplices"
-  where
-    eqType :: (Typeable x, Typeable y) => q x -> Set (s y) -> Maybe (x :~: y)
-    eqType _ _ = eqT
 
 -----------------------------------------------------------------------------------------
 -- Chain -
@@ -160,3 +236,4 @@ c   = complex [Set [0..5]] :: Complex N
 h   = homology $ chainComplexFree' (Proxy :: Proxy [])  Regular (attest :: Any N5) c
 
 
+-}
