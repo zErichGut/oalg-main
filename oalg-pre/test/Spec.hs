c77204694cbf67b0b6d28622ac0696d7a9699899
zErichGut <121794449+zErichGut@users.noreply.github.com>
2025-12-11 07:20:27 -0600

    test suite

--- oalg-pre/test/Spec.hs
+++ oalg-pre/test/Spec.hs
@@ -1,2 +1,29 @@
+
+{-# LANGUAGE NoImplicitPrelude #-}
+
+import Control.Monad
+
+import Data.List((++))
+
+import OAlg.Prelude
+
+proposition :: Statement
+proposition = Prp "oalg-pre"
+  :<=>: And [ prpBool
+            , prpValidTautologies
+            , prpStatement
+            ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  b <- validateStatistics Sparse proposition
+
+  putStrLn ""
+  putStrLn "***************************"
+  putStrLn ("Result     " ++ show b)
+  putStrLn "***************************"
+  putStrLn ""
+  if b < ProbablyValid
+    then error (show b)
+    else return ()
+
--- oalg-stc/test/Spec.hs
+++ oalg-stc/test/Spec.hs
@@ -1,2 +1,23 @@
+
+{-# LANGUAGE NoImplicitPrelude #-}
+
+import Control.Monad
+
+import Data.List((++))
+
+import OAlg.Prelude
+import OAlg.Structure.Proposition
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  b <- validateStatistics Sparse prpStructure
+
+  putStrLn ""
+  putStrLn "***************************"
+  putStrLn ("Result     " ++ show b)
+  putStrLn "***************************"
+  putStrLn ""
+  if b < ProbablyValid
+    then error (show b)
+    else return ()
+
