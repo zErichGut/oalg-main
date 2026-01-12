8d9d1519f82508086d2b3902f44bd1b1a713526d
zErichGut <121794449+zErichGut@users.noreply.github.com>
2026-01-12 04:01:16 -0600

    work on cnzNormalFormTo

--- oalg-lin-alg/src/OAlg/LinearAlgebra/ConsecutiveZero.hs
+++ oalg-lin-alg/src/OAlg/LinearAlgebra/ConsecutiveZero.hs
@@ -119,13 +119,13 @@ dgzField :: Field k => Diagonalizable k
 dgzField = Diagonalizable mtxDiagonalForm
 
 --------------------------------------------------------------------------------
--- isoChainDiagFst -
+-- invChainDiagFst -
 
--- | see 'isoChainDiagFst'.
-isoChainToDiagFst :: Distributive k
+-- | see 'invChainDiagFst'.
+invChainToDiagFst :: Distributive k
   => Diagonalizable k -> Diagram (Chain To) (n+1) n (Matrix k)
   -> Inv (DiagramTrafo (Chain To) (n+1) n (Matrix k))
-isoChainToDiagFst dgz a@(DiagramChainTo _ chs) = case chs of
+invChainToDiagFst dgz a@(DiagramChainTo _ chs) = case chs of
   Nil     -> one a
   m:|chs' -> Inv t f where
     
@@ -148,11 +148,11 @@ isoChainToDiagFst dgz a@(DiagramChainTo _ chs) = case chs of
 
 -- | isomorphism from the given chain diagram to a chain diagram with first matrix a diagonal matrix
 -- with non zero entries.
-isoChainDiagFst :: Distributive k
+invChainDiagFst :: Distributive k
   => Diagonalizable k -> Diagram (Chain t) (n+1) n (Matrix k)
   -> Inv (DiagramTrafo (Chain t) (n+1) n (Matrix k))
-isoChainDiagFst d a     = case a of
-  DiagramChainTo _ _   -> isoChainToDiagFst d a
+invChainDiagFst d a     = case a of
+  DiagramChainTo _ _   -> invChainToDiagFst d a
   DiagramChainFrom _ _ -> error "nyi"
 
 --------------------------------------------------------------------------------
@@ -277,42 +277,64 @@ instance Distributive k => Validable (ConsZeroNormalForm t n k) where
 --     @il@ is the first row index of @dl@ with a entry not equal to 'zero' and
 --     @rk@ is the last row index of @dk@  with a entry not equal to 'zero' (note: if @dk@ is
 --     is 'zero', then @rk@ is defined as @-1@.)
-cnzNormalFormTo :: (Galoisian k, Monic k, Attestable n)
+cnzNormalFormTo :: (Galoisian k, Monic k)--, Attestable n)
   => Diagonalizable k
+  -> Any n
   -> ConsecutiveZero To n (Matrix k) -> Inv (ConsecutiveZeroHom To n (Matrix k))
-cnzNormalFormTo dgz (ConsecutiveZero c0) = Inv t f where
+cnzNormalFormTo dgz n (ConsecutiveZero c0) = case n' of {Ats -> toCnzInv (inv1 * inv0)} where
+  n' = ats n
 
-  t = error "nyi"
-  f = error "nyi"
-
-  Inv t0 f0 = isoChainToDiagFst dgz c0
+  inv0@(Inv t0 f0) = invChainToDiagFst dgz c0
   -- start t0 is equal to c0.
 
-  c1 = end t0
+  c1 = case n' of Ats -> end t0
   d0:|d1:|ds = dgArrows c1
 
   -- rank of d0.
   r = lengthN $ mtxxs d0
+  r' = dim unit ^ r
+  
   -- as d0 is diagonal and the ring k is monic, it follows that all rows of d1
   -- with a row index i < r are zero!
   s = (lengthN $ start d0) >- r
+  s' = dim unit ^ s
 
-  p1 = mtxJoin $ matrixBlc [s'] [r',s'] [(one s',0,1)] where
-    r' = dim unit ^ r
-    s' = dim unit ^ s
-
+  os = one s'
+  p1 = mtxJoin $ matrixBlc [s'] [r',s'] [(os,0,1)]
+  i1 = mtxJoin $ matrixBlc [r',s'] [s'] [(os,1,0)]
+    
   -- tail of c1 with adapted first matrix
   c'1 = DiagramChainTo (end d'1) (d'1 :| ds) where d'1 = p1 * d1
 
-  Inv t'1 f'1  = isoChainToDiagFst dgz c'1
-  c'2 = end t'1
-    
+  Inv t'1 f'1  = nfTo dgz (SW n) c'1
+  c'2 = case n' of Ats -> end t'1
 
-{-
-    chs'' = case chs' of
-      Nil   -> Nil
-      _:|cs -> c':|cs 
--}
+  c2 = DiagramChainTo (end d0) (d0:|d'1:|ds) where
+    d'1    = d1 * i1
+    d1:|ds = dgArrows c'2
+
+  inv1 = Inv t1 f1 where
+    t1 = DiagramTrafo c1 c2 fs where fs = error "nyi"
+    f1 = DiagramTrafo c2 c1 fs where fs = error "nyi"
+
+  
+  -- pre: if n >= 2, then c is consecutive zero!
+  nfTo :: (Galoisian k, Monic k)
+    => Diagonalizable k
+    -> Any n
+    -> Diagram (Chain To) (n+1) n (Matrix k) -> Inv (DiagramTrafo (Chain To) (n+1) n (Matrix k))
+  nfTo dgz (SW (SW n)) c  = Inv t' f' where
+    Inv t f               = cnzNormalFormTo dgz n (ConsecutiveZero c)
+    ConsecutiveZeroHom t' = t
+    ConsecutiveZeroHom f' = f
+  nfTo dgz _ c            = invChainToDiagFst dgz c
+
+  toCnzInv :: Inv (DiagramTrafo (Chain To) (n+3) (n+2) (Matrix k))
+           -> Inv (ConsecutiveZeroHom To n (Matrix k))
+  toCnzInv (Inv t f) = Inv t' f' where
+    t' = ConsecutiveZeroHom t
+    f' = ConsecutiveZeroHom f
+
 
 mt :: (Ring r, i ~ N, j ~ N) => N -> N -> [([(r,j)],i)] -> Matrix r
 mt r c xijs = matrixTtl r c xijs' where
@@ -332,4 +354,4 @@ d = DiagramChainTo (end m) (m:|Nil)
 xx :: X (Diagram (Chain To) N4 N3 (Matrix Q))
 xx = xStandard
 
-pp = Forall xx (valid . isoChainDiagFst dgzField)
+pp = Forall xx (valid . invChainDiagFst dgzField)
