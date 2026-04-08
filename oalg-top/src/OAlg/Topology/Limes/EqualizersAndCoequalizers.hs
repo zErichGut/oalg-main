26cb51290b303415502c0fdb304a6b7a10f3a1a0
zErichGut <121794449+zErichGut@users.noreply.github.com>
2026-04-08 05:16:12 -0600

    work on coequalizer

--- oalg-top/src/OAlg/Topology/Limes/EqualizersAndCoequalizers.hs
+++ oalg-top/src/OAlg/Topology/Limes/EqualizersAndCoequalizers.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DataKinds, TupleSections #-}
@@ -24,6 +25,8 @@ module OAlg.Topology.Limes.EqualizersAndCoequalizers
 
 -- import Data.Typeable
 
+import Data.List (zip)
+
 import OAlg.Prelude
 
 -- import OAlg.Category.Map
@@ -32,15 +35,17 @@ import OAlg.Prelude
 -- import OAlg.Data.Filterable
 
 import OAlg.Structure.Oriented
--- import OAlg.Structure.Additive
+import OAlg.Structure.Additive
 import OAlg.Structure.Multiplicative
+import OAlg.Structure.Ring
 -- import OAlg.Structure.PartiallyOrdered
 
 -- import OAlg.Entity.Diagram
 import OAlg.Entity.Natural
-import OAlg.Entity.FinList as F
+import OAlg.Entity.FinList as F hiding (zip)
+import OAlg.Entity.Sequence.PSequence
 import OAlg.Entity.Sequence.Set
--- import OAlg.Entity.Sequence.Graph
+import OAlg.Entity.Sequence.Graph
 
 import OAlg.Entity.Matrix
 
@@ -53,30 +58,80 @@ import OAlg.Limes.Exact.Deviation
 import OAlg.Homology.Simplical hiding (simplex,dimension)
 import OAlg.Homology.Complex -- hiding (cpxProduct, cpxProductAsc)
 import OAlg.Homology.ChainComplex
-import OAlg.Homology.Definition hiding (B)
+import OAlg.Homology.Definition hiding (B,D,F)
 
 import OAlg.Topology.Definition
 -- import OAlg.Topology.Limes.TerminalAndInitialSpace
 
 import OAlg.Data.Symbol
---------------------------------------------------------------------------------
--- ff -
 
--- | the various conection classes.
-ff :: Space m -> Matrix F2
-ff u@(SpaceConcrete _) = ff $ spcAbstract u
-ff u@(SpaceAbstract _)   = p * v where
+--------------------------------------------------------------------------------
+-- Vertex -
+
+data Vertex where Vertex :: (Entity x, Ord x) => x -> Vertex
+
+--------------------------------------------------------------------------------
+-- connectionClasses -
+
+-- | the various connection classes.
+connectionClasses :: Space m -> Matrix F2
+connectionClasses u@(SpaceConcrete _) = connectionClasses $ spcAbstract u
+connectionClasses u@(SpaceAbstract cx) = p * v where
   p = cokernelFactor $ universalCone ckr
   v = universalFactor kr (ConeKernel d o) where
     d = universalDiagram kr
     o = one $ end $ kernelFactor $ universalCone kr
 
   VarianceG _ ((kr,ckr):|Nil) = homology f2 cf
-  
-  cf = pmap (hF f2 . hZ . hC) u
+
+  cc = chainComplex ChainComplexStandard SpxTypeSet (attest :: Any N0) cx
+  cf = pmap (hF f2 . hZ) cc
   f2 = HmlgF :: f ~ F2 => Homological f (Matrix f)
-  hC = hC' SpxTypeSet f2 ChainComplexStandard (attest :: Any N0)
 
+--------------------------------------------------------------------------------
+-- psqList -
+
+-- | the list given by a partially defined sequence with the given length, where the omitted elements
+-- are set by the given default value.
+psqList :: N -> x -> PSequence N x -> [x]
+psqList iL xD (PSequence xis) = psq 0 xD xis where
+  psq :: N -> x -> [(x,N)] -> [x]
+  psq i _ _ | iL <= i        = []
+  psq i xD []                = xD : psq (i+1) xD []
+  psq i xD xis@((x,i'):xis') = if i < i'
+    then (xD:psq (i+1) xD xis)
+    else (x:psq (i+1) xD xis')
+
+--------------------------------------------------------------------------------
+-- mtxColVecs -
+
+-- | the columns as a list of vectors.
+mtxColVecs :: Semiring x => Matrix x -> [Vector x]
+mtxColVecs m = vcs (cols m) (mtxRowCol m) where
+  vcs :: Semiring x => Dim' x -> Row N (Col N x) -> [Vector x]
+  vcs cls (Row xjs) = psqList (lengthN cls) (zero ()) $ psqMap toVec xjs
+
+  toVec :: Col N x -> Vector x
+  toVec (Col xis) = Vector xis
+   
+--------------------------------------------------------------------------------
+-- cpxConnectionGraph -
+
+cpxConnectionGraph :: (Entity x, Ord x) => Complex x -> Graph x (Vector F2)
+cpxConnectionGraph cx = Graph (vx `zip` mtxColVecs (p * v)) where
+  vx = setxs $ cpxVertices cx
   
+  p = cokernelFactor $ universalCone ckr
+  v = universalFactor kr (ConeKernel d o) where
+    d = universalDiagram kr
+    o = one $ end $ kernelFactor $ universalCone kr
 
-s = SpaceAbstract $ complex [Set [A,B],Set [C]] 
+  VarianceG _ ((kr,ckr):|Nil) = homology f2 cf
+
+  cc = chainComplex ChainComplexStandard SpxTypeSet (attest :: Any N0) cx
+  cf = pmap (hF f2 . hZ) cc
+  f2 = HmlgF :: f ~ F2 => Homological f (Matrix f)
+  
+--------------------------------------------------------------------------------
+-- 
+s = complex [Set [A,B],Set [B,C],Set [D,E],Set [F]] 
